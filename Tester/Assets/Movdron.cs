4b4247db3488e51db803d0ee54fb1cdafd12f8a5
SergioDDiaz <61720992+SergioDDiaz@users.noreply.github.com>
2020-05-30 19:24:16 -0500

    funcionamiento del dron

---
+++ Tester/Assets/Movdron.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movdron : MonoBehaviour
+{
+    Vector3 posicion;
+    private float grav = 0.1f;
+    void FixedUpdate()
+    {
+        posicion = gameObject.GetComponent<Transform>().position;
+        float an3 = Input.GetAxis("Gatillo");
+
+        if (posicion.y >= 0)
+        {
+            posicion.y -= grav;
+
+            if (an3 > 0.5)
+            {
+                posicion.y += an3;
+            }
+
+        }
+        else
+            posicion.y = 0;
+        gameObject.GetComponent<Transform>().position = posicion;
+    }
+
+}
---
+++ Tester/Assets/RotationHelice.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationHelice : MonoBehaviour
+{
+    public int type;
+    private float speed, stat;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float an3 = Input.GetAxis("Gatillo");
+
+        if (an3 > 0.05 && speed <= 50)
+        {
+            speed = 50 * an3;
+            if (type == 1 || type == 3)
+            {
+                speed = -1 * speed;
+                transform.Rotate(0, speed, 0);
+
+            }
+            if (type == 2 || type == 4)
+            {
+
+                transform.Rotate(0, speed, 0);
+
+            }
+            stat = speed;
+        }
+        speed = stat;
+    }
+}
